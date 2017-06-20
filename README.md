e4697470aded010a5281da8c044b1bd4021d716a
Aliaksei <matsukaliaksei@gmail.com>
2017-06-20 12:19:15 +0300

    first commit in this project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Matsuk Aliaksei 
+> The build works for gulp version 4.0. 
+
+#### To get started
+
+1. ```clone this repo```
+2. ```cd path/to/liveHTML```
+3. ```npm install gulpjs/gulp-cli -g```
+4. ```npm install```
+6. ```run gulp``` 
+
+#### or open index.html from build folder
---
+++ build/assets/js/app.js
@@ -0,0 +1,5 @@
+(function() {
+  'use strict';
+
+})();
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24oKSB7XHJcbiAgJ3VzZSBzdHJpY3QnO1xyXG5cclxufSkoKTsiXX0=
